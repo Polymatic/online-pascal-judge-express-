4d998c92ae617eaa1297222f07d3e76870fd155e
Mridul Garg <mridulgarg.mg@gmail.com>
2020-08-11 15:31:03 +0530

    Update execute.js

--- backend/execute.js
+++ backend/execute.js
@@ -10,22 +10,17 @@ const interpreteScript = (res, script, language) => {
   const python = spawn('python3', [DIR, script])
   
   python.stderr.on('data', function (data) {
-    console.log('stderr:-------------------------------------', data.toString());
-    // dataToSend = data.toString();
-    returnObject.stderr = data.toString()
+    console.log('stderr:', data.toString());
     returnObject.stdout = ""
    })
 
   python.stdout.on('data', function (data) {
     console.log('stdout:', data.toString());
-    // dataToSend = data.toString();
     returnObject.stdout = data.toString()
    })
 
   python.on('close', (code) => {
      console.log(`child process close all stdio with code ${code}`);
-     // console.log('inside runPythonScript func',dataToSend)
-
      res.json(returnObject)
      });
 }
